a8c5d1251e625a0ae3f2a32be6f45886b814bd0c
Tin Nguyen <tnguyen@itq.lan>
2025-09-18 21:39:37 +0200

    separate the files

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 8080;
 
 // Serve static files from the 'web' directory
-app.use(express.static(path.join(__dirname, 'web')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.listen(PORT, () => {
     console.log(`Static site served at http://localhost:${PORT}`);
