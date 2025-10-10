43785825bd8552bc55abbd5838b29f07e045c638
Tin Nguyen <tnguyen@itq.lan>
2025-10-10 19:20:31 +0200

    address review

--- tests/websocket-utils.test.ts
+++ tests/websocket-utils.test.ts
@@ -4,8 +4,8 @@
  */
 
 import { beforeEach, describe, expect, it, vi } from 'vitest';
-import { createJoinServerMessage, getStartupServer, util, type DiscordAuth, type GameState } from '../src/websocket-utils';
-import { MockLocalStorage } from './utils/testHelpers';
+import { createJoinServerMessage, getStartupServer, util, type DiscordAuth, type GameState } from '../src/websocket-utils.js';
+import { MockLocalStorage } from './utils/testHelpers.js';
 
 // Create instance of mock localStorage
 const mockLocalStorage = new MockLocalStorage();
